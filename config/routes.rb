32b140870f2e214781951736df69f2dd0e0da664
Leon <mgleon08@gmail.com>
2016-03-21 22:20:28 +0800

    backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'page#index'
-  resources :posts
+  resources :posts, only: [:index, :show]
   get 'templates', to:'page#template'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -51,9 +51,10 @@ Rails.application.routes.draw do
   #   resources :photos, concerns: :toggleable
 
   # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+    namespace :backend do
+      get '/' => 'page#index'
+      # Directs /admin/products/* to Admin::ProductsController
+      # (app/controllers/admin/products_controller.rb)
+      resources :posts
+    end
 end
